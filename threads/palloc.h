20ddf8ba0166884f49ff343fedd040ff182e9e86
hj52470 <jhj33175247@gmail.com>
2025-12-13 02:37:03 +0900

    Fix header guard in palloc.h

--- threads/palloc.h
+++ threads/palloc.h
@@ -26,4 +26,4 @@ void *palloc_get_multiple(enum palloc_flags, size_t page_cnt);
 void palloc_free_page(void *);
 void palloc_free_multiple(void *, size_t page_cnt);
 
-#endif /* threads/palloc.h */
+#endif /* threa*
