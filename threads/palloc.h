d169b5a2a8e0a4aa6818417ab1e7c33c3750edba
hj52470 <jhj33175247@gmail.com>
2025-12-13 02:29:32 +0900

    Update palloc.h

--- threads/palloc.h
+++ threads/palloc.h
@@ -3,18 +3,27 @@
 
 #include <stddef.h>
 
-/* How to allocate pages. */
-enum palloc_flags
-{
-    PAL_ASSERT = 001, /* Panic on failure. */
-    PAL_ZERO = 002,   /* Zero page contents. */
-    PAL_USER = 004    /* User page. */
+/* Contiguous allocation mode selector */
+enum palloc_mode {
+    PAL_FIRST_FIT,
+    PAL_NEXT_FIT,
+    PAL_BEST_FIT,
+    PAL_BUDDY
 };
 
-void palloc_init (size_t user_page_limit);
-void *palloc_get_page (enum palloc_flags);
-void *palloc_get_multiple (enum palloc_flags, size_t page_cnt);
-void palloc_free_page (void *);
-void palloc_free_multiple (void *, size_t page_cnt);
+void palloc_set_mode(enum palloc_mode mode);
 
-#endif /* threads/palloc.h */
+/* How to allocate pages. */
+enum palloc_flags {
+    PAL_ASSERT = 001,
+    PAL_ZERO   = 002,
+    PAL_USER   = 004
+};
+
+void palloc_init(size_t user_page_limit);
+void *palloc_get_page(enum palloc_flags);
+void *palloc_get_multiple(enum palloc_flags, size_t page_cnt);
+void palloc_free_page(void *);
+void palloc_free_multiple(void *, size_t page_cnt);
+
+#endif
